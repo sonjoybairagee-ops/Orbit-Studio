0da8a388d3b9499ecaed1cb803ae1e27bc4fddf4
Sonjoy <sonjoy.bairagee@gmail.com>
2026-07-26 21:28:53 +0600

    Clean up test files

--- test-r2-env2.js
+++
@@ -1,7 +0,0 @@
-﻿const fs = require('fs');
-const env = fs.readFileSync('.env.local', 'utf8').split('\n').reduce((acc, line) => {
-  const match = line.match(/^([^=]+)=(.*)$/);
-  if (match) acc[match[1]] = match[2];
-  return acc;
-}, {});
-console.log('Secret end with \\r?', env.R2_SECRET_ACCESS_KEY?.endsWith('\r'));
--- update-env-fix.js
+++
@@ -1,6 +0,0 @@
-﻿const fs = require('fs');
-let env = fs.readFileSync('.env.local', 'utf8');
-
-env = env.replace(/^R2_ACCESS_KEY_ID=.*$/m, 'R2_ACCESS_KEY_ID=c9d00ce7158df187c4451a4ca39ef602');
-
-fs.writeFileSync('.env.local', env, 'utf8');
